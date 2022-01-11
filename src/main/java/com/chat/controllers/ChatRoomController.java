066c5752ed375cb9281012fd35dc444798a775f8
minhchien <minhchien0407.vk.1718@gmail.com>
2022-01-11 09:38:38 +0700

    add css button ok of nicknam_form

--- src/main/java/com/chat/controllers/ChatRoomController.java
+++ src/main/java/com/chat/controllers/ChatRoomController.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 
 public class ChatRoomController implements Initializable {
     public Client client;
@@ -34,19 +35,16 @@ public class ChatRoomController implements Initializable {
     public NicknameFormController nicknameFormController;
 
     @FXML
-    private TextField tf_message;
+    private TextField tf_message, timeTextField;
 
     @FXML
     private VBox vbox_messages;
 
     @FXML
-    private Label labelClientName;
+    private Label labelClientName, labelMyName;
 
     @FXML
     private ScrollPane sp_main;
-
-    @FXML
-    private Label labelMyName;
 
     public void receive() throws IOException {
         new Thread() {
@@ -57,6 +55,9 @@ public class ChatRoomController implements Initializable {
                             break;
                         }
                         data = client.getRev().receive();
+
+                        if (data == null) break;
+
                         String message = (String) data.get("message");
                         Platform.runLater(() -> {
                             String status = (String) data.get("status");
@@ -120,54 +121,134 @@ public class ChatRoomController implements Initializable {
 
         if (message.isEmpty()) {
             AlertUtils.showWarning("Hãy nhập tin nhắn");
+            tf_message.setFocusTraversable(true);
             return;
-        } else {
-            showMessage(message);
-            tf_message.clear();
-
-            data.put("message", message);
-            client.getSend().sendData(data);
         }
+
+        showMessage(message);
+        tf_message.clear();
+        tf_message.setFocusTraversable(true);
+
+        data.put("message", message);
+        client.getSend().sendData(data);
     }
 
     public void showMessage(String message) {
         HBox hBox = new HBox();
         hBox.setAlignment(Pos.CENTER_RIGHT);
-        hBox.setPadding(new Insets(5, 5, 5, 10));
+        hBox.setPadding(new Insets(1, 5, 1, 80));
 
         Text text = new Text(message);
         text.setFill(Color.color(0.934, 0.935, 0.996));
-        text.setFont(Font.font("Segoe UI Historic", 15));
+        text.setFont(Font.font("Segoe UI Historic", 14));
 
         TextFlow textFlow = new TextFlow(text);
         textFlow.getStyleClass().add("text-flow-show-message");
-        textFlow.setPadding(new Insets(5, 10, 5, 10));
+        textFlow.setPadding(new Insets(8, 12, 8, 12));
 
         hBox.getChildren().add(textFlow);
         vbox_messages.getChildren().add(hBox);
+
+        int sizeVbox = vbox_messages.getChildren().size();
+
+        if (sizeVbox >= 2) {
+            HBox hbox = (HBox) vbox_messages.getChildren().get(sizeVbox - 1);
+
+            if (((HBox) (vbox_messages.getChildren().get(sizeVbox - 2))).getAlignment().equals(Pos.CENTER_RIGHT)) {
+                ((HBox) (vbox_messages.getChildren().get(sizeVbox - 2))).getChildren().get(0).getStyleClass().add("text-flow-show-message-pre");
+                hbox.getChildren().get(0).getStyleClass().add("text-flow-show-message-curr");
+            } else {
+                hBox.setPadding(new Insets(10, 5, 1, 80));
+                return;
+            }
+
+            if (sizeVbox >= 3) {
+                for (int i = sizeVbox - 2; i >= 1; i--) {
+                    HBox hBox1 = (HBox) vbox_messages.getChildren().get(i);
+                    int j = i;
+                    if (((HBox) (vbox_messages.getChildren().get(j - 1))).getAlignment().equals(Pos.CENTER_RIGHT)) {
+                        ((HBox) (vbox_messages.getChildren().get(j - 1))).getChildren().get(0).getStyleClass().add("text-flow-show-message-pre");
+                        hBox1.getChildren().get(0).getStyleClass().add("text-flow-show-message-between");
+                        continue;
+                    }
+                    break;
+                }
+            }
+        }
+
         vbox_messages.heightProperty().addListener(observable -> sp_main.setVvalue(1D));
     }
 
     public void showMessageReceive(String message) {
         HBox hBox = new HBox();
         hBox.setAlignment(Pos.CENTER_LEFT);
-        hBox.setPadding(new Insets(5, 5, 5, 10));
+        hBox.setPadding(new Insets(1, 80, 1, 5));
 
         Text text = new Text(message);
         text.setFill(Color.BLACK);
-        text.setFont(Font.font("Segoe UI Historic", 15));
+        text.setFont(Font.font("Segoe UI Historic", 14));
 
         TextFlow textFlow = new TextFlow(text);
         textFlow.getStyleClass().add("text-flow-show-message-receive");
-        textFlow.setPadding(new Insets(5, 10, 5, 10));
+        textFlow.setPadding(new Insets(8, 12, 8, 12));
 
         hBox.getChildren().add(textFlow);
         vbox_messages.getChildren().add(hBox);
+
+        int sizeVbox = vbox_messages.getChildren().size();
+
+        if (sizeVbox >= 2) {
+            HBox hbox = (HBox) vbox_messages.getChildren().get(sizeVbox - 1);
+
+            if (((HBox) (vbox_messages.getChildren().get(sizeVbox - 2))).getAlignment().equals(Pos.CENTER_LEFT)) {
+                ((HBox) (vbox_messages.getChildren().get(sizeVbox - 2))).getChildren().get(0).getStyleClass().add("text-flow-show-message-receive-pre");
+                hbox.getChildren().get(0).getStyleClass().add("text-flow-show-message-receive-curr");
+            } else {
+                hBox.setPadding(new Insets(10, 80, 1, 5));
+                return;
+            }
+
+            if (sizeVbox >= 3) {
+                for (int i = sizeVbox - 2; i >= 1; i--) {
+                    HBox hBox1 = (HBox) vbox_messages.getChildren().get(i);
+                    int j = i;
+                    if (((HBox) (vbox_messages.getChildren().get(j - 1))).getAlignment().equals(Pos.CENTER_LEFT)) {
+                        ((HBox) (vbox_messages.getChildren().get(j - 1))).getChildren().get(0).getStyleClass().add("text-flow-show-message-receive-pre");
+                        hBox1.getChildren().get(0).getStyleClass().add("text-flow-show-message-receive-between");
+                        continue;
+                    }
+                    break;
+                }
+            }
+        }
+
         vbox_messages.heightProperty().addListener(observable -> sp_main.setVvalue(1D));
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        vbox_messages.setPadding(new Insets(5, 0, 5, 0));
+        new Thread(() -> {
+            Long startMillis = System.currentTimeMillis();
+            while (true) {
+                Long millis = System.currentTimeMillis() - startMillis;
+                String hms = String.format("%02d:%02d:%02d",
+                        TimeUnit.MILLISECONDS.toHours(millis),
+                        TimeUnit.MILLISECONDS.toMinutes(millis) -
+                                TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                        TimeUnit.MILLISECONDS.toSeconds(millis) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+                Platform.runLater(() -> {
+                    timeTextField.setText(hms);
+                });
+                try {
+                    Thread.sleep(1000); //1 second
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
         if (data.get("message") != "") {
             showMessage(data.get("message").toString());
         }
